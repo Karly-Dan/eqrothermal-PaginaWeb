40213c3995760b6483ce7adddce7ce5010bd536b
Karly-Dan <daniela.kar99@gmail.com>
2023-09-05 22:02:10 -0600

    se agrega configuracion para envio de formulario

---
+++ pages/nosotros.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <header-vue/>
+    <div>
+        <p>Estamos ocupados en generar confiabilidad con nuestros clientes cumpliendo con sus expectativas. </p>
+    </div>
+  </main>
+</template>
+
+<script>
+import headerVue from '~/components/header.vue'
+
+export default {
+    components: {
+        headerVue,
+    }
+}
+</script>
+
+<style>
+
+</style>
