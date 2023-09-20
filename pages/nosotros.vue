badfe1b52bc8434d4fb4bfdb3a9bd1a8514cec54
Karly-Dan <daniela.kar99@gmail.com>
2023-09-19 18:35:54 -0600

    se agrega informacion para pagina nosotros

--- pages/nosotros.vue
+++ pages/nosotros.vue
@@ -4,6 +4,31 @@
     <div>
         <p>Estamos ocupados en generar confiabilidad con nuestros clientes cumpliendo con sus expectativas. </p>
     </div>
+    <div>
+      <div>
+        <h1>Misión:</h1>
+        <p>Retar los tiempos de entrega establecidos por la industria siendo la calidad 
+        en los acabados superficiales superior a los procesos tradicionales, 
+        a través de equipos eléctricos sustentables que nos permiten gracias 
+        a su tecnología gestionar las cargas de forma eficiente dando como resultado
+        tratamientos térmicos de primera clase.</p>
+      </div>
+      <div>
+        <h1>Visión:</h1>
+        <p>Lograr ser la empresa de tratamientos térmicos que entregue el mejor
+        servicio de valor agregado al producto que permita a nuestros clientes mantenerse
+        a la vanguardia, a través de las energías limpias, la automatización y la tecnología. </p>
+      </div>
+      <div>
+        <h1>Nuestros Principios:</h1>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
   </main>
 </template>
 
