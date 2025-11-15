1d78e743002ec8d4c4c9b3c392ae444d69d682e0
vikoss <vikoss.yiss@gmail.com>
2025-11-14 20:48:29 -0600

    feat: add env variables to nuxt file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    SENDGRID_EMAIL_FROM: process.env.SENDGRID_EMAIL_FROM,
-    APP_URL: process.env.APP_URL,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_EMAIL_FROM: process.env.SMTP_EMAIL_FROM,
+    public: {
+      APP_URL: process.env.APP_URL,
+    },
   },
 })
